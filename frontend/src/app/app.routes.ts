0976415ce6031f357feaf7a03e79e878178b1f52
Gary H <3rdeye.ghj@gmail.com>
2026-03-17 11:35:28 -0700

    all page files duplicated with updated class names and routing logic added to routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Portfolio } from './pages/portfolio/portfolio';
+import { Contact } from './pages/contact/contact';
+import { Blog } from './pages/blog/blog';
 
 export const routes: Routes = [
-  { path: '', component: Home }
+  { path: '', component: Home },
+  { path: 'portfolio', component: Portfolio },
+  { path: 'contact', component: Contact },
+  { path: 'certification', component: Contact},
+  { path: 'blog', component: Blog }
 ];
---
+++ frontend/src/app/pages/certifications/certifications.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-certifications',
+  imports: [],
+  templateUrl: './certifications.html',
+  styleUrl: './certifications.css'
+})
+export class Certifications {
+
+}
+
