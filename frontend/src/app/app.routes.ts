3c53434c0741c7f8b9d560437d63dbfb09b749c5
Gary H <3rdeye.ghj@gmail.com>
2026-03-17 13:23:21 -0700

    layout for each homepage section added and styled

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { Home } from './pages/home/home';
 import { Portfolio } from './pages/portfolio/portfolio';
 import { Contact } from './pages/contact/contact';
 import { Blog } from './pages/blog/blog';
+import { Certifications } from './pages/certifications/certifications';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'portfolio', component: Portfolio },
   { path: 'contact', component: Contact },
-  { path: 'certification', component: Contact},
+  { path: 'certifications', component: Certifications },
   { path: 'blog', component: Blog }
 ];
